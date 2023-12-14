ab6023fcfaf1caf24cc56850f5fb027bdb46f0ad
Obake18 <miltondesenhista@gmail.com>
2023-12-14 13:53:13 -0300

    Criados os artistras

--- src/app/layout.js
+++ src/app/layout.js
@@ -28,7 +28,7 @@ function Albuns({ generos }) {
   );
 }
 
-function Seekbar({ capa, icon2, botao }) {
+function Seekbar({ capa, icon2}) {
   return (
     <div className='seekbar'>
       <div className='seekbar-info'>
@@ -55,6 +55,40 @@ function Seekbar({ capa, icon2, botao }) {
   );
 }
 
+const artistasData = [
+  {
+    id: 1,
+    genero: 'Pop',
+    tipo: 'Música',
+    imagem: 'url_da_imagem_artista_1.jpg',
+  },
+  {
+    id: 2,
+    genero: 'Hip-Hop',
+    tipo: 'Música',
+    imagem: 'url_da_imagem_artista_2.jpg',
+  },
+  {
+    id: 3,
+    genero: 'Podcast',
+    tipo: 'Podcast',
+    imagem: 'url_da_imagem_artista_3.jpg',
+  
+  },
+]
+function Artistas() {
+  return (
+    <div className="artistas-container">
+      {artistasData.map((artista) => (
+        <div key={artista.id} className="artista-item">
+          <img src={artista.imagem} alt={`Imagem de ${artista.genero}`} />
+          <p>Gênero: {artista.genero}</p>
+          <p>Tipo: {artista.tipo}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
 
 
 export default function CustomLayout({ children, backgroundColor }) {
@@ -130,11 +164,96 @@ export default function CustomLayout({ children, backgroundColor }) {
     'Grime',
     'Trap',
     'Chiptune',
-    'Game Music',
-    'Idol Pop',
-    'Anison',
-    'Vocaloid',
-    'Karaoke',
+    'Game Music',￼Home
+
+    ￼Search
+    
+    ￼Your Library
+    
+    ￼Criar Playlist
+    
+    ￼Seus Favoritos
+    
+    J-Pop
+    Anime Soundtracks
+    J-Rock
+    Electronic
+    Hip-Hop
+    Enka
+    Traditional Japanese
+    Indie
+    Kawaii Metal
+    City Pop
+    Visual Kei
+    Kayokyoku
+    Jazz
+    Classical
+    Folk
+    Ska
+    Reggae
+    Punk
+    Metal
+    Techno
+    House
+    Trance
+    Ambient
+    Fusion
+    World Music
+    Experimental
+    Noise
+    Post-Rock
+    Post-Punk
+    Shibuya-Kei
+    Electropop
+    Art Pop
+    Dream Pop
+    Shoegaze
+    Alternative Rock
+    Emo
+    Math Rock
+    Post-Hardcore
+    Hardcore Punk
+    Gothic Rock
+    Industrial
+    New Wave
+    Synthpop
+    Disco
+    Funk
+    Soul
+    R&B
+    Blues
+    Rockabilly
+    Country
+    Bluegrass
+    Latin
+    Hip-Hop Instrumental
+    Beats
+    Breakbeat
+    Drum and Bass
+    Dubstep
+    Grime
+    Trap
+    Chiptune
+    Game Music
+    Idol Pop
+    Anison
+    Vocaloid
+    Karaoke
+    Taiko
+    Shamisen
+    Koto
+    Shakuhachi
+    Biwa
+    Sanshin
+    Okinawan Folk
+    Ryukyu Classical
+    Gagaku
+    ￼
+    Gênero: Pop
+    
+    Tipo: Música
+    
+    
     'Taiko',
     'Shamisen',
     'Koto',
@@ -174,7 +293,9 @@ export default function CustomLayout({ children, backgroundColor }) {
           </div>
         </div>
         <div className='Fundo-2'>
+        <Artistas>
 
+        </Artistas>
         </div>
 
         <div>
